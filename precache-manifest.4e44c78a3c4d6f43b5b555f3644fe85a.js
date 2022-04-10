4bd80d43ea6b2dd3e099346b6f0fb821e36b04b8
edson <3685020900101@ingenieria.usac.edu.gt>
2022-04-10 00:50:49 -0600

    Updates

---
+++ precache-manifest.4e44c78a3c4d6f43b5b555f3644fe85a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "03ef95ce14ca8a667d43",
+    "url": "/portafolio/static/css/main.3cf38261.chunk.css"
+  },
+  {
+    "revision": "03ef95ce14ca8a667d43",
+    "url": "/portafolio/static/js/main.03ef95ce.chunk.js"
+  },
+  {
+    "revision": "bb0898e875365f106adb",
+    "url": "/portafolio/static/js/1.bb0898e8.chunk.js"
+  },
+  {
+    "revision": "4c8085b5d7a51c1f8344",
+    "url": "/portafolio/static/css/2.886d09af.chunk.css"
+  },
+  {
+    "revision": "4c8085b5d7a51c1f8344",
+    "url": "/portafolio/static/js/2.4c8085b5.chunk.js"
+  },
+  {
+    "revision": "02fff18e7347749e8452",
+    "url": "/portafolio/static/js/runtime~main.02fff18e.js"
+  },
+  {
+    "revision": "56deb4c774b8e173827052d0583a2eaf",
+    "url": "/portafolio/static/media/photo.56deb4c7.png"
+  },
+  {
+    "revision": "75839d3c62021ddd251c7905d539ae21",
+    "url": "/portafolio/index.html"
+  }
+];
